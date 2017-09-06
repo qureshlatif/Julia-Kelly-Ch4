2c59f475b6d99e80584664d925f995f7ef3a1ecc
Quresh Latif <qureshlatif@yahoo.com>
2017-09-06 15:38:04 -0600

    adjusts plots following Ibis review
    Adds alternative plot with finite sample occupancy estimates for site X year occasions.
    Provides predicted occupancy for min and max QMD values to report in Results.

--- Results.R
+++ Results.R
@@ -35,8 +35,10 @@ dimnames(EarlInf.x)[[2]] <- dimnames(MidInf.x)[[2]] <- dimnames(snag.x)[[2]] <- 
   c("2013","2014","2015","2016")
 
 # Correlation matrix #
-#cor(cbind(EarlInf=as.numeric(EarlInf.x),MidInf=as.numeric(MidInf.x),snag=as.numeric(snag.x),
-#          QMD=as.numeric(QMD.x)))
+#covTab <- cbind(EarlInf=as.numeric(EarlInf.x),MidInf=as.numeric(MidInf.x),snag=as.numeric(snag.x),
+#          QMD=as.numeric(QMD.x))
+#covTab <- covTab[-which(is.na(Y)),]
+#cor(covTab)
 
 ###---Analysis of occupancy (assumes closure between visits within a year)---###
 Y <- apply(Y.arry,c(1,2,3),function(x) 1*(x>0)) # Collapse counts to detection-nondetection data
@@ -55,7 +57,9 @@ Z.init <- apply(Y,c(1,2),function(x) sum(x>0)) # Initial values for occupancy st
 
 mod <- loadObject("Mod_Pers_EInfMInfSnagQMD")
 #mod <- loadObject("Mod_Pers_EInfMInfSnagQMD_PriorTrunc")
-write.csv(mod$BUGSoutput$summary,"Model_summary.csv") # Export summary of model parameters and output.
+write.csv(mod$BUGSoutput$summary[
+  -which(substr(dimnames(mod$BUGSoutput$summary)[[1]], 1, 1) == "Z"), ],
+          "Model_summary.csv") # Export summary of model parameters and output.
 
 #mod <- loadObject("Mod_Pers_EInfMInfSnagQMD_noClrCut")
 #write.csv(mod$BUGSoutput$summary,"Model_summary_noClrCut.csv") # Export summary of model parameters and output.
@@ -65,40 +69,48 @@ library(ggplot2)
 library(cowplot)
 expit <- function(x) exp(x)/(1+exp(x))
 
-## Plot covariate parameter estimates ##
+## Plot covariate parameter estimates (Reviewer asked for table, so dropping this plot.) ##
 
-rows <- c("beta0","beta1","EarlInf","MidInf","Snag","QMD")
-cols <- c("med","lo","hi")
+rows <- c("beta0","beta0+beta1","EarlInf","MidInf","Snag","QMD")
+cols <- c("med","lo95","lo90","hi90","hi95")
 dat <- data.frame(matrix(0,nrow=length(rows),ncol=length(cols),dimnames=list(rows,cols)))
 
 dat$med <- apply(mod$BUGSoutput$sims.array[,,c("beta0","beta1","beta.EarlInf","beta.MidInf",
-                                                   "beta.snag","beta.QMD")],3,median)
-dat$lo <- apply(mod$BUGSoutput$sims.array[,,c("beta0","beta1","beta.EarlInf","beta.MidInf",
+                                                   "beta.snag","beta.QMD")], 3, median)
+dat$lo95 <- apply(mod$BUGSoutput$sims.array[,,c("beta0","beta1","beta.EarlInf","beta.MidInf",
                                                    "beta.snag","beta.QMD")],3,function(x)
-                                                     quantile(x,prob=0.025))
-dat$hi <- apply(mod$BUGSoutput$sims.array[,,c("beta0","beta1","beta.EarlInf","beta.MidInf",
+                                                     quantile(x, prob=0.025, type = 8))
+dat$hi95 <- apply(mod$BUGSoutput$sims.array[,,c("beta0","beta1","beta.EarlInf","beta.MidInf",
                                                    "beta.snag","beta.QMD")],3,function(x)
-                                                     quantile(x,prob=0.975))
-dat$X <- c(6.5,5.5,4:1)
+                                                     quantile(x, prob=0.975, type = 8))
+dat$lo90 <- apply(mod$BUGSoutput$sims.array[,,c("beta0","beta1","beta.EarlInf","beta.MidInf",
+                                                "beta.snag","beta.QMD")],3,function(x)
+                                                  quantile(x, prob=0.05, type = 8))
+dat$hi90 <- apply(mod$BUGSoutput$sims.array[,,c("beta0","beta1","beta.EarlInf","beta.MidInf",
+                                                "beta.snag","beta.QMD")],3,function(x)
+                                                  quantile(x, prob=0.95, type = 8))
 
-p <- ggplot(data = dat,aes(x=X,y=med)) +
-  geom_errorbar(aes(ymin=lo,ymax=hi),size=1,width=0) +
-  geom_point(size=2.5) + 
-  geom_hline(yintercept=0,linetype="dotted") +
-  coord_flip() +
-  scale_x_continuous(breaks=c(1:4,5.5,6.5),labels=c(expression(beta["QMD"]),
-                                                     expression(beta["sng"]),
-                                                     expression(beta["Minf"]),
-                                                     expression(beta["Einf"]),
-                                                     expression(beta[1]),
-                                                     expression(beta[0]))) +
-  ylab("Parameter estimate") + xlab("              Covariate effects                             Intercept terms") +
-  theme(axis.title.y=element_text(size=30)) +
-  theme(axis.title.x=element_text(size=30)) +
-  theme(axis.text.x=element_text(size=25)) +
-  theme(axis.text.y=element_text(size=25))
+dat <- round(dat, digits = 2)
+write.csv(dat, "Model_summary_MS.csv", row.names = T)
+rm(dat, b0b1)
+#p <- ggplot(data = dat,aes(x=X,y=med)) +
+#  geom_errorbar(aes(ymin=lo,ymax=hi),size=1,width=0) +
+#  geom_point(size=2.5) + 
+#  geom_hline(yintercept=0,linetype="dotted") +
+#  coord_flip() +
+#  scale_x_continuous(breaks=c(1:4,5.5,6.5),labels=c(expression(beta["QMD"]),
+#                                                     expression(beta["sng"]),
+#                                                     expression(beta["Minf"]),
+#                                                     expression(beta["Einf"]),
+#                                                     expression(beta[1]),
+#                                                     expression(beta[0]))) +
+#  ylab("Parameter estimate") + xlab("              Covariate effects                             Intercept terms") +
+#  theme(axis.title.y=element_text(size=30)) +
+#  theme(axis.title.x=element_text(size=30)) +
+#  theme(axis.text.x=element_text(size=25)) +
+#  theme(axis.text.y=element_text(size=25))
 
-save_plot("Occupancy_parameters.jpeg", p, ncol = 3, nrow = 3, dpi=600) 
+#save_plot("Occupancy_parameters.jpeg", p, ncol = 3, nrow = 3, dpi=600) 
 
 # Early infestation #
   # finite-sample estimates #
@@ -165,3 +177,89 @@ p <- ggplot(data = dat.prd,aes(x=X,y=psi)) +
   guides(shape=FALSE,linetype=FALSE)
 
 save_plot("EarlInf_relation.jpeg", p, ncol = 3, nrow = 3, dpi=600) 
+
+## With finite-sample estimates for sites and years ##
+# Compile site names for siteXyear occupancy estimates (requested by reviewer) #
+require(stringr)
+sites <- str_sub(plot, 1, -3)
+sites <- sites %>% replace(which(sites == "11"), "TV") %>%
+  replace(which(sites %in%c("1", "2")), "SG") %>%
+  replace(which(sites == "4"), "WT") %>%
+  replace(which(sites == "5"), "SM")
+
+# site X year estimates #
+Z.est <- mod$BUGSoutput$sims.list$Z
+
+Sites <- rep(unique(sites), 4)
+Years <- rep(year, each = 4)
+dat.SXY <- data.frame(cbind(Sites,Years), stringsAsFactors = F)
+dat.SXY$Years <- as.numeric(dat.SXY$Years)
+dat.SXY$psi.hi <- dat.SXY$psi.lo <- dat.SXY$psi <- dat.SXY$X <- 0
+
+for(s in 1:nrow(dat.SXY)) {
+  ind.s <- which(sites == dat.SXY$Sites[s])
+  ind.yr <- which(year == as.numeric(dat.SXY$Years[s]))
+  dat.SXY[s, "X"] <- mean(EarlInf.x[ind.s, ind.yr])
+  psifs <- apply(Z.est[, ind.s, ind.yr], 1, sum) / length(ind.s)
+  dat.SXY[s, "psi"] <- median(psifs)
+  dat.SXY[s, "psi.lo"] <- quantile(psifs, prob = 0.025, type = 8)
+  dat.SXY[s, "psi.hi"] <- quantile(psifs, prob = 0.975, type = 8)
+}
+dat.SXY$X <- dat.SXY$X * 25 # Convert to per ha
+dat.SXY <- dat.SXY[-which(dat.SXY$Sites == "SG" &
+                            dat.SXY$Years == "2013"), ] # Remove value for Slumgullion in 2013 when not surveyed.
+dat.prd$X <- dat.prd$X * 25 # Convert to per ha
+
+# Tabulate site X year early infestation values #
+require(gridExtra)
+tab.EInf <- cbind(c("-", round(dat.SXY$X[1:3], digits = 1)),
+                  round(dat.SXY$X[4:7], digits = 1),
+                  round(dat.SXY$X[8:11], digits = 1),
+                  round(dat.SXY$X[12:15], digits = 1))
+dimnames(tab.EInf) <- list(c("SG", "TV", "WT", "SM"), c(2013, 2014, 2015, 2016))
+
+theme_set(theme_cowplot())
+p <- ggplot(data = dat.prd,aes(x=X,y=psi)) +
+  geom_line(size=1,linetype="solid") +
+  geom_line(aes(y=psi.lo),size=1,linetype="dashed") +
+  geom_line(aes(y=psi.hi),size=1,linetype="dashed") +
+  geom_point(data=dat.SXY, aes(x=X, y=psi, shape = Sites), size=3) + 
+  geom_errorbar(data=dat.SXY, aes(x=X, ymin=psi.lo, ymax=psi.hi), size=1, width = 0) +
+  ylab("Point occupancy") + xlab("Early Infested Trees (stems/ha)") +
+  scale_y_continuous(lim=c(0,1.05),breaks=c(0,0.25,0.5,0.75,1)) +
+  scale_shape_manual(values = c(15, 16, 17, 18)) +
+  theme(axis.title.x=element_text(size=30)) +
+  theme(axis.title.y=element_text(size=30)) +
+  theme(axis.text.x=element_text(size=20)) +
+  theme(axis.text.y=element_text(size=25)) +
+  guides(linetype=FALSE) +
+  theme(legend.title = element_text(size = 25),
+        legend.text = element_text(size = 20),
+        legend.key.height=unit(1, "cm")) +
+  annotation_custom(tableGrob(tab.EInf, theme = ttheme_minimal()), xmin=600, xmax=1200, ymin=0, ymax=0.5) +
+  annotate("text", x = 900, y = 0.4, label = "Early Infested trees per ha", size = 5)
+
+#save_plot("EarlInf_relation_SXY.jpeg", p, ncol = 3, nrow = 3, dpi=600) 
+save_plot("EarlInf_relation_SXY.tiff", p, ncol = 2, nrow = 2, dpi=600) 
+save_plot("EarlInf_relation_SXY.eps", p, ncol = 3, nrow = 3, dpi=600, device = "eps") 
+
+# Predicted probabilities with QMD (to be reported in Results text) #
+  # Remove observations for pointxyear occassions following clear-cut #
+QMD.x[which(substr(plot, 1, 3) %in% c("1a7","1a8","1a9")), c(3, 4)] <- NA
+QMD.x[which(plot=="2a5"),4] <- NA
+
+X <- c(min(QMD.x, na.rm = T), max(QMD.x, na.rm = T))
+beta0 <- mod$BUGSoutput$sims.list$beta0
+beta1 <- mod$BUGSoutput$sims.list$beta1
+psifs <- mod$BUGSoutput$sims.list$psi.fs
+betaX <- mod$BUGSoutput$sims.list$beta.QMD
+
+Z <- (X - zscore.factors["QMD","mean"])/zscore.factors["QMD","sd"]
+psi <- psi.lo <- psi.hi <- numeric(length=length(X))
+for(i in 1:length(X)) {
+  y <- expit(beta0 + beta1*psifs + betaX*Z[i])
+  psi[i] <- median(y)
+  psi.lo[i] <- quantile(y,prob=0.025,type=8)
+  psi.hi[i] <- quantile(y,prob=0.975,type=8)
+}
+dat.prd2 <- data.frame(cbind(X,psi,psi.lo,psi.hi))
